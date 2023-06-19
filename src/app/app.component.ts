5cbae21f2c7ac115fa021c401cf06f3545181474
Dibyaprakashojha <dibya.prakash04@gmail.com>
2023-06-19 22:05:36 +0530

    second push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'oauth-fe';
+  constructor(private http: HttpClient) {}
+
+  renderText: string = '';
+  ngOnInit() {
+    this.getHello().subscribe({
+      next: (res) => {
+        this.renderText = res;
+        console.log(`Hello World`, res);
+      },
+      error: (err) => {
+        if (err.status == 401) {
+          this.getLoginUri(this.redirectUri).subscribe({
+            next: (response: any) => {
+              console.log(`Url: `, response);
+              window.location.href = response.url;
+            },
+          });
+        }
+        console.log(`error: `, err);
+      },
+    });
+  }
+
+  getHello = () => {
+    const helloUrl = 'http://localhost:8095/api/hello';
+    return this.http.get(helloUrl, {
+      withCredentials: true,
+      responseType: 'text',
+    });
+  };
+  baseUrl: string = 'http://localhost:8095/api/loginUri';
+  redirectUri: string = `http://localhost:8095/api/ticketConsumer?redirectUri=${window.location.href}`;
+  getLoginUri = (redirectUri: string) => {
+    return this.http.get(`${this.baseUrl}?redirectUri=${redirectUri}`);
+  };
 }
